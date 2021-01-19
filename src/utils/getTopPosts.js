9a32d2337230704255d6f272853a26673cd9c82e
Aditya Phra <adit@adityaphra.com>
2021-01-19 13:58:20 +0700

    set time to always 00:01

--- src/utils/getTopPosts.js
+++ src/utils/getTopPosts.js
@@ -3,32 +3,34 @@ import dayjs from 'dayjs';
 import { format } from 'timeago.js';
 import dedent from 'dedent';
 
-const apiUrl =
-  'https://hn.algolia.com/api/v1/search?numericFilters=created_at_i>';
+const apiUrl = 'https://hn.algolia.com/api/v1/search?numericFilters=';
 
 export default async function getTopPosts(username) {
   console.log('fetching posts from hacker news');
 
   // set headline & timestamp
-  let timestamp = dayjs().subtract(1, 'day').unix();
+  const now = dayjs().hour(0).minute(1).second(0).millisecond(0);
+  let timestamp = dayjs(now).subtract(1, 'day').unix();
   let message = 'Daily';
 
   if (username === '@weekly_hackernews') {
-    timestamp = dayjs().subtract(1, 'week').unix();
+    timestamp = dayjs(now).subtract(1, 'week').unix();
     message = 'Weekly';
   }
 
   if (username === '@monthly_hackernews') {
-    timestamp = dayjs().subtract(1, 'month').unix();
+    timestamp = dayjs(now).subtract(1, 'month').unix();
     message = 'Monthly';
   }
 
-  const date = dayjs().format('DD-MM-YYYY');
+  const date = dayjs(now).format('DD-MM-YYYY');
 
   const headline = `<strong>Hacker News ${message} Top 10 @${date}</strong>`;
 
   // fetch data from algolia
-  const response = await axios.get(`${apiUrl}${timestamp}`);
+  const response = await axios.get(
+    `${apiUrl}created_at_i>${timestamp},created_at_i<${now.unix()}`,
+  );
 
   const result = response.data.hits
     .filter((post) => post.title != null)
